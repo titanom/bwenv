523795f3f438d08eb0c6a28ea93e0900bc718223
Jannis Morgenstern <nyarthan@proton.me>
2023-08-28 19:02:56 +0200

    feat: parse token & default to env var

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+use clap::{Arg, Command};
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("bwenv")
+        .about("TODO")
+        .version("TODO")
+        .arg_required_else_help(true)
+        .author("nyarthan")
+        .arg(
+            Arg::new("token")
+                .long("token")
+                .short('t')
+                .env("BWS_ACCESS_TOKEN"),
+        )
+        .get_matches();
+
+    if let Some(token) = matches.get_one::<String>("token") {
+        println!("Token: {}", token);
+    }
 }
